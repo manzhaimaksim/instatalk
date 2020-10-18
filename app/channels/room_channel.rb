e75b6664ed00babe1c4c206b24713fb1b46edb41
manzhaimaksim <maxbbb88@gmail.com>
2020-10-18 20:49:55 +0300

    separate messages from room to room

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,6 +1,8 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    logger.info "Subscribed to RoomChannel"
+    logger.info "Subscribed to RoomChannel, roomId: #{params[:roomId]}"
+
+    @room = Room.find(params[:roomId])
 
     stream_from 'room_channel'
   end
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,22 +1,30 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-    console.log('Connected to RoomChannel')
-  },
+let room_id;
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-    console.log('Disconnected from RoomChannel')
+$(document).on('turbolinks:load', function () {
+  consumer.subscriptions.create({
+    channel: "RoomChannel",
+    roomId: $('#messages').attr('data-room-id')
   },
+  {
+    connected() {
+      // Called when the subscription is ready for use on the server
+      console.log('Connected to RoomChannel')
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    console.log('Received data: ' + data['message'])
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+      console.log('Disconnected from RoomChannel')
+    },
 
-  speak: function() {
-    return this.perform('speak');
-  }
-});
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      console.log('Received data: ' + data['message'])
+    },
+
+    speak: function() {
+      return this.perform('speak');
+    }
+  });
+})
